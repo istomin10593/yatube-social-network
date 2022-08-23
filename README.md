bdd87c3461ac78e25dba3186e995b84e706cd29f
istomin10593 <istomin.artem.10593@gmail.com>
2022-08-23 11:45:39 +0000

    changed readme

--- README.md
+++ README.md
@@ -55,11 +55,34 @@ The application is fully covered by Unit Tests.
   pip install -r requirements.txt
 ```
 
+  * Migration:
 
-  * Run server and project:
+  
 
 ```
-  python manage.py runserver
+
+  python manage.py makemigrations
+
+```
+
+  
+
+```
+
+  python manage.py migrate
+
+```
+
+  
+
+* Run server and project:
+
+  
+
+```
+
+python manage.py runserver
+
 ```
 ## 	&#129299; About Me
 
--- yatube/settings.py
+++ yatube/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+from dotenv import load_dotenv
+load_dotenv()
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '@bsx0gq-vjzf9-je@7-3f2xuy%j5cr^f)!^pwveci911ab!4v3'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
